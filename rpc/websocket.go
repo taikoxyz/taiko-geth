3c802e24fbfd98ecfae722f77ba367a636edd318
David <david@taiko.xyz>
2023-01-06 13:04:55 +0800

    feat(ws): remove ratelimit

--- rpc/websocket.go
+++ rpc/websocket.go
@@ -38,7 +38,7 @@ const (
 	wsPingInterval     = 60 * time.Second
 	wsPingWriteTimeout = 5 * time.Second
 	wsPongTimeout      = 30 * time.Second
-	wsMessageSizeLimit = 0 * time.Second
+	wsMessageSizeLimit = 0
 )
 
 var wsBufferPool = new(sync.Pool)
