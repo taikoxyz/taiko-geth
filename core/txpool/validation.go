7ae4d8448784f44e70776d4765b0b36089362131
David <david@taiko.xyz>
2023-10-15 23:02:58 +0800

    feat(all): changes based on Taiko protocol

--- core/txpool/validation.go
+++ core/txpool/validation.go
@@ -18,8 +18,10 @@ package txpool
 
 import (
 	"crypto/sha256"
+	"errors"
 	"fmt"
 	"math/big"
+	"os"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core"
@@ -90,6 +92,11 @@ func ValidateTransaction(tx *types.Transaction, blobs []kzg4844.Blob, commits []
 	if tx.GasFeeCapIntCmp(tx.GasTipCap()) < 0 {
 		return core.ErrTipAboveFeeCap
 	}
+	// CHANGE(taiko): ensure gasFeeCap fee cap is not zero
+	if os.Getenv("TAIKO_TEST") == "" && tx.GasFeeCap().Cmp(common.Big0) == 0 {
+		return errors.New("max fee per gas is zero")
+	}
+
 	// Make sure the transaction is signed properly
 	if _, err := types.Sender(signer, tx); err != nil {
 		return ErrInvalidSender
