002515bd29ab05795b087b5f632139f79af957bb
Jeffery Walsh <cyberhorsey@gmail.com>
2024-07-25 12:06:58 -0700

    .

--- internal/ethapi/preconf.go
+++ internal/ethapi/preconf.go
@@ -77,6 +77,11 @@ func forward[T any](forwardURL string, method string, params []interface{}) (*T,
 		return nil, fmt.Errorf("RPC error %d: %s", rpcResp.Error.Code, rpcResp.Error.Message)
 	}
 
+	if rpcResp.Result == nil {
+		log.Info("forwarded request result is nil", "method", method)
+		return nil, nil
+	}
+
 	log.Info("forwarded request result", "method", method, "res", rpcResp.Result, "type", reflect.TypeOf(rpcResp.Result).String())
 
 	return rpcResp.Result.(*T), nil
