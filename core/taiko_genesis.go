0f2bd905554bf75d1cdbe4771b69104709075516
David <david@taiko.xyz>
2024-08-28 17:21:15 +0800

    Update core/taiko_genesis.go
    Co-authored-by: Gavin Yu <gavin@taiko.xyz>

--- core/taiko_genesis.go
+++ core/taiko_genesis.go
@@ -11,7 +11,7 @@ import (
 
 var (
 	InternalDevnetOntakeBlock = new(big.Int).SetUint64(2)
-	HeklaOntakeBlock          = new(big.Int).SetUint64(720_000)
+	HeklaOntakeBlock          = new(big.Int).SetUint64(793_000)
 	MainnetOntakeBlock        = new(big.Int).SetUint64(374_400)
 )
 
