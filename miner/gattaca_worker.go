502db9257cb7bc63e146ba6e91e2c4f72d8abf12
fbrv <fabio@gattaca.com>
2024-09-16 14:43:17 +0100

    logs

--- miner/gattaca_worker.go
+++ miner/gattaca_worker.go
@@ -484,6 +484,7 @@ func (g *GattacaWorker) sealBlock(req SealBlockRequest) {
 	g.preconfHead.header.MixDigest = g.mixHash
 	g.preconfHead.header.Extra = make([]byte, 32)
 	log.Info("Header extra data is", "extra-data", len(g.preconfHead.header.Extra), "content", g.preconfHead.header.Extra)
+	log.Info("number of transaction per block", "blockNo", g.preconfHead.header.Number.Uint64(), "transaction count", len(g.preconfHead.txs))
 	block, err := g.engine.FinalizeAndAssemble(g.chain, g.preconfHead.header, g.preconfHead.state, g.preconfHead.txs, nil, g.preconfHead.receipts, make([]*types.Withdrawal, 0))
 	if err != nil {
 
