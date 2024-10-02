cab3cace900cf79776b28fab92b2eadf39a7f65e
maskpp <maskpp266@gmail.com>
2024-10-02 16:38:40 +0800

    fix TestBuildTransactionsLists test (#327)

--- miner/taiko_worker_test.go
+++ miner/taiko_worker_test.go
@@ -2,15 +2,37 @@ package miner
 
 import (
 	"math/big"
+	"os"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus/clique"
 	"github.com/ethereum/go-ethereum/core/rawdb"
+	"github.com/ethereum/go-ethereum/core/txpool"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/stretchr/testify/assert"
 )
+
+const (
+	// testCode is the testing contract binary code which will initialises some
+	// variables in constructor
+	testCode = "0x60806040527fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0060005534801561003457600080fd5b5060fc806100436000396000f3fe6080604052348015600f57600080fd5b506004361060325760003560e01c80630c4dae8814603757806398a213cf146053575b600080fd5b603d607e565b6040518082815260200191505060405180910390f35b607c60048036036020811015606757600080fd5b81019080803590602001909291905050506084565b005b60005481565b806000819055507fe9e44f9f7da8c559de847a3232b57364adc0354f15a2cd8dc636d54396f9587a6000546040518082815260200191505060405180910390a15056fea265627a7a723058208ae31d9424f2d0bc2a3da1a5dd659db2d71ec322a17db8f87e19e209e3a1ff4a64736f6c634300050a0032"
+
+	// testGas is the gas required for contract deployment.
+	testGas = 144109
+)
+
+func newRandomTx(txPool *txpool.TxPool, creation bool) *types.Transaction {
+	var tx *types.Transaction
+	gasPrice := big.NewInt(10 * params.InitialBaseFee)
+	if creation {
+		tx, _ = types.SignTx(types.NewContractCreation(txPool.Nonce(testBankAddress), big.NewInt(0), testGas, gasPrice, common.FromHex(testCode)), types.HomesteadSigner{}, testBankKey)
+	} else {
+		tx, _ = types.SignTx(types.NewTransaction(txPool.Nonce(testBankAddress), testUserAddress, big.NewInt(1000), params.TxGas, gasPrice, nil), types.HomesteadSigner{}, testBankKey)
+	}
+	return tx
+}
 
 func testGenerateWorker(t *testing.T, txCount int) *Miner {
 	t.Parallel()
@@ -24,18 +46,16 @@ func testGenerateWorker(t *testing.T, txCount int) *Miner {
 
 	w, b := newTestWorker(t, &config, engine, db, 0)
 
-	insert := make([]*types.Transaction, txCount)
-	for nonce := range insert {
-		tx := types.NewTransaction(uint64(nonce), common.Address{}, big.NewInt(0), 100000, big.NewInt(0), make([]byte, 10240))
-		tx, _ = types.SignTx(tx, types.HomesteadSigner{}, testBankKey)
-		insert[nonce] = tx
+	for i := 0; i < txCount; i++ {
+		b.txPool.Add([]*types.Transaction{newRandomTx(b.txPool, true)}, true, false)
+		b.txPool.Add([]*types.Transaction{newRandomTx(b.txPool, false)}, true, false)
 	}
-	b.txPool.Add(insert, true, false)
 
 	return w
 }
 
 func TestBuildTransactionsLists(t *testing.T) {
+	os.Setenv("TAIKO_TEST", "true")
 	w := testGenerateWorker(t, 1000)
 
 	maxBytesPerTxList := (params.BlobTxBytesPerFieldElement - 1) * params.BlobTxFieldElementsPerBlob
