87f395e9f629c38ac5b56c6e332562267f9baf1a
Jeffery Walsh <cyberhorsey@gmail.com>
2025-02-20 10:02:54 -0800

    update taik oauth apis

--- eth/taiko_api_backend.go
+++ eth/taiko_api_backend.go
@@ -60,18 +60,6 @@ func (s *TaikoAPIBackend) L1OriginByID(blockID *math.HexOrDecimal256) (*rawdb.L1
 	return l1Origin, nil
 }
 
-// SetHeadL1Origin sets the latest L2 block's corresponding L1 origin.
-func (s *TaikoAPIBackend) SetHeadL1Origin(blockID *math.HexOrDecimal256) *big.Int {
-	rawdb.WriteHeadL1Origin(s.eth.ChainDb(), (*big.Int)(blockID))
-	return (*big.Int)(blockID)
-}
-
-// UpdateL1Origin updates the L2 block's corresponding L1 origin.
-func (s *TaikoAPIBackend) UpdateL1Origin(l1Origin *rawdb.L1Origin) *rawdb.L1Origin {
-	rawdb.WriteL1Origin(s.eth.ChainDb(), l1Origin.BlockID, l1Origin)
-	return l1Origin
-}
-
 // GetSyncMode returns the node sync mode.
 func (s *TaikoAPIBackend) GetSyncMode() (string, error) {
 	return s.eth.config.SyncMode.String(), nil
@@ -144,4 +132,16 @@ func (a *TaikoAuthAPIBackend) TxPoolContentWithMinTip(
 		maxTransactionsLists,
 		minTip,
 	)
+}
+
+// SetHeadL1Origin sets the latest L2 block's corresponding L1 origin.
+func (a *TaikoAuthAPIBackend) SetHeadL1Origin(blockID *math.HexOrDecimal256) *big.Int {
+	rawdb.WriteHeadL1Origin(a.eth.ChainDb(), (*big.Int)(blockID))
+	return (*big.Int)(blockID)
+}
+
+// UpdateL1Origin updates the L2 block's corresponding L1 origin.
+func (a *TaikoAuthAPIBackend) UpdateL1Origin(l1Origin *rawdb.L1Origin) *rawdb.L1Origin {
+	rawdb.WriteL1Origin(a.eth.ChainDb(), l1Origin.BlockID, l1Origin)
+	return l1Origin
 }
