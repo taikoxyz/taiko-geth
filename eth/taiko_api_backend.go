9c22c53301048cb99f7651199e0c21e937dca138
Jeffery Walsh <cyberhorsey@gmail.com>
2025-02-27 09:42:06 -0800

    api

--- eth/taiko_api_backend.go
+++ eth/taiko_api_backend.go
@@ -46,16 +46,18 @@ func (s *TaikoAPIBackend) HeadL1Origin() (*rawdb.L1Origin, error) {
 	return l1Origin, nil
 }
 
-// SetHeadL1Origin sets the latest L2 block's corresponding L1 origin.
-func (a *TaikoAuthAPIBackend) SetHeadL1Origin(blockID *math.HexOrDecimal256) *big.Int {
-	rawdb.WriteHeadL1Origin(a.eth.ChainDb(), (*big.Int)(blockID))
-	return (*big.Int)(blockID)
-}
+// L1OriginByID returns the L2 block's corresponding L1 origin.
+func (s *TaikoAPIBackend) L1OriginByID(blockID *math.HexOrDecimal256) (*rawdb.L1Origin, error) {
+	l1Origin, err := rawdb.ReadL1Origin(s.eth.ChainDb(), (*big.Int)(blockID))
+	if err != nil {
+		return nil, err
+	}
 
-// UpdateL1Origin updates the L2 block's corresponding L1 origin.
-func (a *TaikoAuthAPIBackend) UpdateL1Origin(l1Origin *rawdb.L1Origin) *rawdb.L1Origin {
-	rawdb.WriteL1Origin(a.eth.ChainDb(), l1Origin.BlockID, l1Origin)
-	return l1Origin
+	if l1Origin == nil {
+		return nil, ethereum.NotFound
+	}
+
+	return l1Origin, nil
 }
 
 // GetSyncMode returns the node sync mode.
@@ -71,6 +73,18 @@ type TaikoAuthAPIBackend struct {
 // NewTaikoAuthAPIBackend creates a new TaikoAuthAPIBackend instance.
 func NewTaikoAuthAPIBackend(eth *Ethereum) *TaikoAuthAPIBackend {
 	return &TaikoAuthAPIBackend{eth}
+}
+
+// SetHeadL1Origin sets the latest L2 block's corresponding L1 origin.
+func (a *TaikoAuthAPIBackend) SetHeadL1Origin(blockID *math.HexOrDecimal256) *big.Int {
+	rawdb.WriteHeadL1Origin(a.eth.ChainDb(), (*big.Int)(blockID))
+	return (*big.Int)(blockID)
+}
+
+// UpdateL1Origin updates the L2 block's corresponding L1 origin.
+func (a *TaikoAuthAPIBackend) UpdateL1Origin(l1Origin *rawdb.L1Origin) *rawdb.L1Origin {
+	rawdb.WriteL1Origin(a.eth.ChainDb(), l1Origin.BlockID, l1Origin)
+	return l1Origin
 }
 
 // TxPoolContent retrieves the transaction pool content with the given upper limits.
