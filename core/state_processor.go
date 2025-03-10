a5f77768901fa976e15edd03faa66e794444c7fa
David <david@taiko.xyz>
2025-03-10 18:26:09 +0900

    feat: update core/state_processor.go

--- core/state_processor.go
+++ core/state_processor.go
@@ -101,6 +101,9 @@ func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg
 		if err != nil {
 			return nil, fmt.Errorf("could not apply tx %d [%v]: %w", i, tx.Hash().Hex(), err)
 		}
+		if p.config.IsOntake(block.Number()) {
+			msg.BasefeeSharingPctg = DecodeOntakeExtraData(header.Extra)
+		}
 		statedb.SetTxContext(tx.Hash(), i)
 
 		receipt, err := ApplyTransactionWithEVM(msg, gp, statedb, blockNumber, blockHash, tx, usedGas, evm)
