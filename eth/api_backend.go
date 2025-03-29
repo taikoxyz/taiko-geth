f6c0793cf5ef64621c31c428b8c17c50ebab2227
David <david@taiko.xyz>
2025-03-29 13:15:18 +0800

    feat: improve RPC

--- eth/api_backend.go
+++ eth/api_backend.go
@@ -187,6 +187,9 @@ func (b *EthAPIBackend) Pending() (*types.Block, types.Receipts, *state.StateDB)
 }
 
 func (b *EthAPIBackend) StateAndHeaderByNumber(ctx context.Context, number rpc.BlockNumber) (*state.StateDB, *types.Header, error) {
+	if number == rpc.PendingBlockNumber {
+		number = rpc.LatestBlockNumber
+	}
 	// Pending state is only known by the miner
 	if number == rpc.PendingBlockNumber {
 		block, _, state := b.eth.miner.Pending()
