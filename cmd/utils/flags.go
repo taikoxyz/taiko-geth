853c91aee7e288ec5e30cf6819b1a245e75aed1b
Dominik Thalmann <dominik@gattaca.com>
2025-04-12 15:25:59 +0200

    - Introduced a new SimulatorAPI for simulating Ethereum transactions and managing pre-confirmed states. - Facilitates testing and development by allowing transaction and block simulations without affecting the main blockchain. - Simulate Transactions: Allows users to simulate transactions in a pre-confirmed state to test their effects without committing them to the blockchain. - Seal Blocks: Provides functionality to seal blocks in the pre-confirmed state, allowing for testing of block finalization processes. - Enable the Simulation API by starting the Ethereum client with the --http.api simulator flag.

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -1894,7 +1894,7 @@ func SetDNSDiscoveryDefaults(cfg *ethconfig.Config, genesis common.Hash) {
 
 // RegisterEthService adds an Ethereum client to the stack.
 // The second return value is the full node instance.
-func RegisterEthService(stack *node.Node, cfg *ethconfig.Config) (*eth.EthAPIBackend, *eth.Ethereum) {
+func RegisterEthService(stack *node.Node, cfg *ethconfig.Config) (ethapi.Backend, *eth.Ethereum) {
 	backend, err := eth.New(stack, cfg)
 	if err != nil {
 		Fatalf("Failed to register the Ethereum service: %v", err)
@@ -1904,7 +1904,7 @@ func RegisterEthService(stack *node.Node, cfg *ethconfig.Config) (*eth.EthAPIBac
 }
 
 // RegisterEthStatsService configures the Ethereum Stats daemon and adds it to the node.
-func RegisterEthStatsService(stack *node.Node, backend *eth.EthAPIBackend, url string) {
+func RegisterEthStatsService(stack *node.Node, backend ethapi.Backend, url string) {
 	if err := ethstats.New(stack, backend, backend.Engine(), url); err != nil {
 		Fatalf("Failed to register the Ethereum Stats service: %v", err)
 	}
--- internal/ethapi/backend.go
+++ internal/ethapi/backend.go
@@ -29,6 +29,7 @@ import (
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/bloombits"
 	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/core/txpool"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/ethdb"
@@ -95,6 +96,9 @@ type Backend interface {
 	SubscribeLogsEvent(ch chan<- []*types.Log) event.Subscription
 	BloomStatus() (uint64, uint64)
 	ServiceFilter(ctx context.Context, session *bloombits.MatcherSession)
+
+	// Common methods between SimulatorBackend and EthAPIBackend
+	TxPool() *txpool.TxPool
 }
 
 func GetAPIs(apiBackend Backend) []rpc.API {
