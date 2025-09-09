e98b41fef914512333ce365c196b71cc03d01aab
David <david@taiko.xyz>
2025-09-09 21:55:20 +0800

    feat: update UpdateStateSelector

--- consensus/taiko/consensus.go
+++ consensus/taiko/consensus.go
@@ -327,12 +327,10 @@ func (t *Taiko) ValidateAnchorTx(tx *types.Transaction, header *types.Header) (b
 
 	if t.chainConfig.IsShasta(header.Number) {
 		if !bytes.HasPrefix(tx.Data(), UpdateStateSelector) {
-			log.Info("Invalid anchor tx data prefix", "data", common.Bytes2Hex(tx.Data()), "selector", common.Bytes2Hex(UpdateStateSelector), "number", header.Number)
 			return false, nil
 		}
 	} else if t.chainConfig.IsPacaya(header.Number) {
 		if !bytes.HasPrefix(tx.Data(), AnchorV3Selector) {
-			log.Info("Invalid anchor tx data prefix2", "data", common.Bytes2Hex(tx.Data()))
 			return false, nil
 		}
 	} else {
@@ -342,39 +340,32 @@ func (t *Taiko) ValidateAnchorTx(tx *types.Transaction, header *types.Header) (b
 	}
 
 	if tx.Value().Cmp(common.Big0) != 0 {
-		log.Info("Invalid anchor tx value", "value", tx.Value())
 		return false, nil
 	}
 
 	if t.chainConfig.IsShasta(header.Number) {
 		if tx.Gas() != UpdateStateGasLimit {
-			log.Info("Invalid anchor tx gas for UpdateState", "gas", tx.Gas())
 			return false, nil
 		}
 	} else if t.chainConfig.IsPacaya(header.Number) {
 		if tx.Gas() != AnchorV3GasLimit {
-			log.Info("Invalid anchor tx gas for AnchorV3", "gas", tx.Gas())
 			return false, nil
 		}
 	} else {
 		if tx.Gas() != AnchorGasLimit {
-			log.Info("Invalid anchor tx gas for Anchor", "gas", tx.Gas())
 			return false, nil
 		}
 	}
 
 	if tx.GasFeeCap().Cmp(header.BaseFee) != 0 {
-		log.Info("Invalid anchor tx gas fee cap", "gasFeeCap", tx.GasFeeCap(), "baseFee", header.BaseFee)
 		return false, nil
 	}
 
 	addr, err := types.MakeSigner(t.chainConfig, header.Number, header.Time).Sender(tx)
 	if err != nil {
-		log.Info("Failed to derive sender of anchor tx", "err", err)
 		return false, err
 	}
 
-	log.Info("Anchor tx sender", "addr", addr.Hex(), "goldenTouch", GoldenTouchAccount.Hex())
 	return strings.EqualFold(addr.String(), GoldenTouchAccount.String()), nil
 }
 
