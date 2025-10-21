2c345660b8c4c53833f54508a9f64538f2110257
David <david@taiko.xyz>
2025-10-21 18:29:38 +0900

    feat: update genesis

--- consensus/taiko/consensus.go
+++ consensus/taiko/consensus.go
@@ -44,9 +44,9 @@ var (
 	AnchorV4Selector = crypto.Keccak256(
 		[]byte("anchorV4((uint48,address,bytes,bytes32,(uint48,uint8,address,address)[]),(uint16,uint48,bytes32,bytes32))"),
 	)[:4]
-	AnchorGasLimit       = uint64(250_000)
-	AnchorV3GasLimit     = uint64(1_000_000)
-	ShastaAnchorGasLimit = uint64(1_000_000)
+	AnchorGasLimit   = uint64(250_000)
+	AnchorV3GasLimit = uint64(1_000_000)
+	AnchorV4GasLimit = uint64(1_000_000)
 )
 
 // Taiko is a consensus engine used by L2 rollup.
@@ -344,7 +344,7 @@ func (t *Taiko) ValidateAnchorTx(tx *types.Transaction, header *types.Header) (b
 	}
 
 	if t.chainConfig.IsShasta(header.Number) {
-		if tx.Gas() != ShastaAnchorGasLimit {
+		if tx.Gas() != AnchorV4GasLimit {
 			return false, nil
 		}
 	} else if t.chainConfig.IsPacaya(header.Number) {
