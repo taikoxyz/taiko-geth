aa7ad6686c4b920784ff365a6493f5bc4bff3edc
David <david@taiko.xyz>
2025-12-25 11:54:32 +0900

    feat: update extradata decode

--- core/state_processor.go
+++ core/state_processor.go
@@ -101,9 +101,7 @@ func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg
 		if err != nil {
 			return nil, fmt.Errorf("could not apply tx %d [%v]: %w", i, tx.Hash().Hex(), err)
 		}
-		if p.config.IsShasta(block.Time()) {
-			msg.BasefeeSharingPctg, _ = DecodeShastaExtraData(header.Extra)
-		} else if p.config.IsOntake(block.Number()) {
+		if p.config.IsOntake(block.Number()) {
 			msg.BasefeeSharingPctg = DecodeOntakeExtraData(header.Extra)
 		}
 		statedb.SetTxContext(tx.Hash(), i)
@@ -218,9 +216,7 @@ func ApplyTransaction(evm *vm.EVM, gp *GasPool, statedb *state.StateDB, header *
 	}
 	// CHANGE(taiko): decode the basefeeSharingPctg config from the extradata, and
 	// add it to the Message, if its an ontake block.
-	if evm.ChainConfig().IsShasta(header.Time) {
-		msg.BasefeeSharingPctg, _ = DecodeShastaExtraData(header.Extra)
-	} else if evm.ChainConfig().IsOntake(header.Number) {
+	if evm.ChainConfig().IsOntake(header.Number) {
 		msg.BasefeeSharingPctg = DecodeOntakeExtraData(header.Extra)
 	}
 	// Create a new context to be used in the EVM environment
--- core/state_transition.go
+++ core/state_transition.go
@@ -709,15 +709,3 @@ func DecodeOntakeExtraData(extradata []byte) uint8 {
 	return uint8(new(big.Int).SetBytes(extradata).Uint64())
 }
 
-// CHANGE(taiko): decodes an Shasta block's extradata, returns basefeeSharingPctg configurations,
-// the corresponding encoding function in protocol is `LibProposing._encodeGasConfigs`.
-func DecodeShastaExtraData(extradata []byte) (uint8, bool) {
-	if len(extradata) < params.ShastaExtraDataLen {
-		return 0, false
-	}
-	// First byte: basefeeSharingPctg
-	basefeeSharingPctg := extradata[0]
-	// Second byte: isLowBondProposal (lowest bit)
-	isLowBondProposal := (extradata[1] & 0x01) != 0
-	return basefeeSharingPctg, isLowBondProposal
-}
