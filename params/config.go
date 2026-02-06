4cae2585e4ccc3c711f10e5f08889417658798e7
David <david@taiko.xyz>
2026-02-06 10:27:58 +0800

    eat(params): include Ontake/Pacaya/Shasta in ChainConfig description

--- params/config.go
+++ params/config.go
@@ -510,6 +510,17 @@ func (c *ChainConfig) Description() string {
 	if c.VerkleTime != nil {
 		banner += fmt.Sprintf(" - Verkle:                      @%-10v\n", *c.VerkleTime)
 	}
+
+	// CHANGE(taiko): add Taiko forks to banner.
+	if c.OntakeBlock != nil {
+		banner += fmt.Sprintf(" - Ontake:                      #%-8v\n", c.OntakeBlock)
+	}
+	if c.PacayaBlock != nil {
+		banner += fmt.Sprintf(" - Pacaya:                      #%-8v\n", c.PacayaBlock)
+	}
+	if c.ShastaTime != nil {
+		banner += fmt.Sprintf(" - Shasta:                      @%-10v\n", *c.ShastaTime)
+	}
 	return banner
 }
 
