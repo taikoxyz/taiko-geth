26f2456f1d30066f5663dbef1ca123cf57451eb4
Gavin “yoghurt” Yu <guoyu960223@gmail.com>
2026-03-12 18:06:43 +0800

    build devnet

--- consensus/misc/taiko_eip4396.go
+++ consensus/misc/taiko_eip4396.go
@@ -97,7 +97,7 @@ func clampEIP4396BaseFeeShasta(config *params.ChainConfig, baseFee *big.Int) *bi
 		return nil
 	}
 	minBaseFee := minBaseFeeShastaDefault
-	if isTaikoMainnet(config) || config.ChainID.Cmp(big.NewInt(167014)) == 0 {
+	if isTaikoMainnet(config) || config.ChainID.Cmp(big.NewInt(167001)) == 0 {
 		minBaseFee = minBaseFeeShastaMainnet
 	}
 	if baseFee.Cmp(minBaseFee) < 0 {
