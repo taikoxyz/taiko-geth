a2f1dd04b823e0c8ae9a12553fd5b44f208232d4
Gavin Yu <gavin@taiko.xyz>
2026-05-06 22:19:54 +0800

    Update core/vm/evm.go

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -355,7 +355,6 @@ func (evm *EVM) Call(caller common.Address, addr common.Address, input []byte, g
 				err = zkErr
 			}
 		}
-
 	} else {
 		// Initialise a new contract and set the code that is to be used by the EVM.
 		code := evm.resolveCode(addr)
